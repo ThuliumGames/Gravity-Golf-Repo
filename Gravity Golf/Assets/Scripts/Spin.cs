a37ddee5cc53061a8129388e0df1d306fbfa7a3f
JamesGagnon1 <43007253+JamesGagnon1@users.noreply.github.com>
2019-05-23 09:18:34 -0400

    more portals and levels

---
+++ Gravity Golf/Assets/Scripts/Spin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+	public float Speed;
+	public float OpeningSpace;
+
+	void Start() {
+		foreach (Transform T in GetComponentInChildren<Transform>()) {
+			if (T.transform != transform) {
+				T.transform.position += -T.transform.up * OpeningSpace;
+			}
+		}
+	}
+	
+
+	void Update () {
+		transform.Rotate(0, 0, Speed , Space.World);
+	}
+}
